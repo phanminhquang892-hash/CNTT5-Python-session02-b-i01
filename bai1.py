c05118399c38280dcc91b4ed089fa4aae01fb1ad
phanminhquang892-hash <phanminhquang892@gmail.com>
2026-05-19 22:15:21 +0700

    Add files via upload

---
+++ bai1.py
@@ -0,0 +1,10 @@
+#Chương trình chạy bình thường nhưng gán sai biến khi xuất dữ liệu, nên gây ra lỗi logic.
+print(' --- HỆ THỐNG TIẾP NHẬN BỆNH NHÂN --- ')
+name_patient = input('Nhập tên bệnh nhân: ')
+age = int(input('Mời bạn nhập tuổi: '))
+symptom = input('Mời bạn nhập triệu chứng bệnh: ')
+
+print(' --- PHIẾU KHÁM BỆNH --- ')
+print ('Tên bệnh nhân:', name_patient)
+print ('Tuổi:', age)
+print ('Triệu chứng:', symptom)
